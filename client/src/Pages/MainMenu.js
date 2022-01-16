7dc694bf7c8b3c0cce5a94194732c55740cfcf44
Ryan-Xu <ryanxu101@gmail.com>
2022-01-16 14:06:00 -0500

    mui

--- client/src/Pages/MainMenu.js
+++ client/src/Pages/MainMenu.js
@@ -1,25 +1,41 @@
 import React from "react";
-import './Pages.css';
+import "./Pages.css";
 
 import { useNavigate } from "react-router-dom";
+import Stack from "@mui/material/Stack";
 
 function MainMenu() {
-
   let navigate = useNavigate();
 
   return (
     <div className="App">
-      <div className="Home-title">
-        <h1>theAPP</h1>
+      <div>
+        <h1 className="Home-title">theAPP</h1>
       </div>
-      <div className="Home-menu">
+
+      <Stack className="Home-menu">
         <header className="App-header">
-          <button className="Menu-button" onClick={() => {navigate("/Calendar")}}>Calendar</button>
-          <button className="Menu-button" onClick={() => {navigate("/ToDoList")}}>To-Do List</button>
-          <br></br><br></br>
+          <button
+            className="Menu-button"
+            onClick={() => {
+              navigate("/Calendar");
+            }}
+          >
+            Calendar
+          </button>
+          <button
+            className="Menu-button"
+            onClick={() => {
+              navigate("/ToDoList");
+            }}
+          >
+            To-Do List
+          </button>
+          <br></br>
+          <br></br>
           {/* <button className="Menu-button">Daily Schedule Tracker</button> */}
         </header>
-      </div>
+      </Stack>
     </div>
   );
 }
